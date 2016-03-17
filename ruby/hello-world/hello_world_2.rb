63b52355d1960a902436a51c169268ce3a1cd486
Rahul Patel <rahulpatel.iiita@gmail.com>
2016-03-17 11:18:06 +0530

    added one more hello world

--- ruby/hello-world/hello_world_2.rb
+++ ruby/hello-world/hello_world_2.rb
@@ -1,6 +1,7 @@
 # Class For Print Hello World
 class HelloWorld
   def self.hello(name = nil)
-    name.nil? ? 'Hello, World!' : 'Hello, ' + name + '!'
+    return 'Hello, World!' if name.nil?
+    'Hello, ' + name + '!'
   end
 end
