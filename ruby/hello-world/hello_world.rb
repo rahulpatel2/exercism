2d323cb17916cfe6ad99b9d5f979efe672139091
Rahul Patel <rahulpatel.iiita@gmail.com>
2016-03-17 11:58:46 +0530

    third hello

--- ruby/hello-world/hello_world.rb
+++ ruby/hello-world/hello_world.rb
@@ -1,6 +1,7 @@
 # Class For Print Hello World
 class HelloWorld
   def self.hello(name = nil)
-    name.nil? ? 'Hello, World!' : 'Hello, ' + name + '!'
+    return 'Hello, World!' if name.nil?
+    return 'Hello, ' + name + '!' unless name.nil?
   end
 end
---
+++ ruby/hello-world/hello_world_3.rb
@@ -0,0 +1,6 @@
+# Class For Print Hello World
+class HelloWorld
+  def self.hello(name = nil)
+    name.nil? ? 'Hello, World!' : 'Hello, ' + name + '!'
+  end
+end
