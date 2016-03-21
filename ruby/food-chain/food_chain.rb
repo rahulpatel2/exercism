bfe6c1da6d1ad30066ca2376f144600d9cda44ee
Rahul Patel <rahulpatel.iiita@gmail.com>
2016-03-21 15:49:51 +0530

    food chain iteration 2

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -2,34 +2,33 @@
 class FoodChain
   VERSION = 2
   def self.song
-    song = []
-    song << "I know an old lady who swallowed a fly.\n"
-    song << "I don't know why she swallowed the fly. Perhaps she'll die.\n\n"
-    song << "I know an old lady who swallowed a spider.\n"
-    song << "It wriggled and jiggled and tickled inside her.\n"
-    song << "She swallowed the spider to catch the fly.\n"
-    song << "I don't know why she swallowed the fly. Perhaps she'll die.\n\n"
+    lyrics = []
+    lyrics << "I know an old lady who swallowed a fly.\n"
+    lyrics << "I don't know why she swallowed the fly. Perhaps she'll die.\n\n"
+    foods = ['spider', 'bird', 'cat', 'dog', 'goat', 'cow']
     second = [
+      "It wriggled and jiggled and tickled inside her.\n",
       "How absurd to swallow a bird!\n",
       "Imagine that, to swallow a cat!\n",
       "What a hog, to swallow a dog!\n",
       "Just opened her throat and swallowed a goat!\n",
       "I don't know how she swallowed a cow!\n"
     ]
-    foods = ['bird', 'cat', 'dog', 'goat', 'cow']
-    (0..4).each do |i|
-      song << "I know an old lady who swallowed a #{foods[i]}.\n"
-      song << second[i]
-      r = (i..1)
+    (0..5).each do |i|
+      lyrics << "I know an old lady who swallowed a #{foods[i]}.\n"
+      lyrics << second[i]
+      r = (i..2)
       r.first.downto(r.last).each do |j|
-        song << "She swallowed the #{foods[j]} to catch the #{foods[j - 1]}.\n"
+        lyrics << "She swallowed the #{foods[j]} to catch the #{foods[j - 1]}.\n"
       end
-      song << "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
-      song << "She swallowed the spider to catch the fly.\n"
-      song << "I don't know why she swallowed the fly. Perhaps she'll die.\n\n"
+      if i != 0
+        lyrics << "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+      end
+      lyrics << "She swallowed the spider to catch the fly.\n"
+      lyrics << "I don't know why she swallowed the fly. Perhaps she'll die.\n\n"
     end
-    song << "I know an old lady who swallowed a horse.\n"
-    song << "She's dead, of course!\n"
-    song.join
+    lyrics << "I know an old lady who swallowed a horse.\n"
+    lyrics << "She's dead, of course!\n"
+    lyrics.join
   end
 end
