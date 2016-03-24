3bef73e3359d6ae4d5bf0da229a04651f3a195e8
Rahul Patel <rahulpatel.iiita@gmail.com>
2016-03-24 11:02:16 +0530

    Acronym In Ruby Iteration -1

---
+++ ruby/acronym/acronym.rb
@@ -0,0 +1,8 @@
+# Program to Convert a long phrase to its acronym
+class Acronym
+  VERSION = 1
+  def self.abbreviate(given)
+    acronym = given.gsub(/(?<=[a-z])(?=[A-Z])/, ' ').tr('-', ' ').split ' '
+    acronym.each_with_object('') { |e, a| a << e[0] }.upcase
+  end
+end
