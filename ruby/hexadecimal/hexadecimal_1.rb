c8e30d917a923f90dd16e56b317f1f5b85104677
Rahul Patel <rahulpatel.iiita@gmail.com>
2016-03-25 13:04:44 +0530

    Hex to decimal iteration 2

--- ruby/hexadecimal/hexadecimal_1.rb
+++ ruby/hexadecimal/hexadecimal_1.rb
@@ -7,17 +7,14 @@ class Hexadecimal
   def to_decimal
     return 0 if @dec_str =~ /[^abcdef0-9]/
     (0...@dec_str.length).inject(0) do |decimal, i|
-      decimal + get_hash_gata(i) * (16**i)
+      num = hex_table[@dec_str[i]].to_i if hex_table.key?(@dec_str[i])
+      num = @dec_str[i].to_i unless hex_table.key?(@dec_str[i])
+      decimal + num * (16**i)
     end
   end
 
   def hex_table
     { 'a' => 10, 'b' => 11, 'c' => 12,
       'd' => 13, 'e' => 14, 'f' => 15 }
-  end
-
-  def get_hash_gata(key)
-    return hex_table[@dec_str[key]].to_i if hex_table.key?(@dec_str[key])
-    @dec_str[key].to_i
   end
 end
