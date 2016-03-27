ce3dc0e5379a811dfed9b16f2fae028bf9b31ab8
Rahul Patel <rahulpatel.iiita@gmail.com>
2016-03-27 14:04:52 +0530

    meetup in ruby iteratio -2

--- ruby/meetup/meetup.rb
+++ ruby/meetup/meetup.rb
@@ -6,13 +6,7 @@ class Meetup
   end
 
   def day(day_name, day_type)
-    if day_type.to_s == 'teenth'
-      Date.new(@year, @month, get_teenth_date(day_name))
-    elsif day_type.to_s == 'last'
-      Date.new(@year, @month, get_last_date(day_name))
-    else
-      Date.new(@year, @month, nth_day(day_name, day_type))
-    end
+    Date.new(@year, @month, nth_day(day_name, day_type))
   end
 
   def get_date(day_name, start_date, end_date)
@@ -21,20 +15,11 @@ class Meetup
     end
   end
 
-  def get_last_date(day_name)
-    end_date = Date.new(@year, @month, -1).day
-    get_date(day_name, end_date - 6, end_date)
-  end
-
-  def get_teenth_date(day_name)
-    get_date(day_name, 13, 19)
-  end
-
   def nth_day(day_name, day_type)
-    return get_date(day_name, 1, 7) if day_type.to_s == 'first'
-    return get_date(day_name, 8, 14) if day_type.to_s == 'second'
-    return get_date(day_name, 15, 21) if day_type.to_s == 'third'
-    get_date(day_name, 22, 28)
+    last = Date.new(@year, @month, -1).day - 6
+    day_hash = { 'first' => 1, 'second' => 8, 'third' => 15,
+                 'fourth' => 22, 'teenth' => 13, 'last' => last }
+    get_date(day_name, day_hash[day_type.to_s], day_hash[day_type.to_s] + 6)
   end
 
   def get_day_name(day)
