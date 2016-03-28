d850aee0d707b9b1b3aaecc4a697c6a2b5f271c9
Rahul Patel <rahulpatel.iiita@gmail.com>
2016-03-28 11:23:05 +0530

    Matrix in ruby iteration -2

--- ruby/matrix/matrix_1.rb
+++ ruby/matrix/matrix_1.rb
@@ -4,13 +4,25 @@ class Matrix
   def initialize(matrix_string)
     @matrix_array = matrix_string.split(/\n/)
     @rows = rows
-    @columns = @rows.transpose
+    @columns = columns
   end
 
   def rows
     temp = []
     @matrix_array.each do |val|
       temp << val.split(' ').map(&:to_i)
+    end
+    temp
+  end
+
+  def columns
+    temp = []
+    (0...@rows[0].length).each do |i|
+      tmp = []
+      (0...@rows.length).each do |j|
+        tmp << @rows[j][i]
+      end
+      temp << tmp
     end
     temp
   end
