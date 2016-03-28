f488124491fcf445c543c549fa9ec3623726c0ca
Rahul Patel <rahulpatel.iiita@gmail.com>
2016-03-28 19:09:01 +0530

    Pig Latin In Ruby iteration -2

--- ruby/pig-latin/pig_latin.rb
+++ ruby/pig-latin/pig_latin.rb
@@ -2,25 +2,20 @@
 class PigLatin
   def self.translate(text)
     @text = text.split(' ')
-    @vowels = %w(a i o u e)
-    output = ''
-    (0...@text.length).each do |i|
+    (0...@text.length).each_with_object('') do |i, output|
       output << convert(@text[i]) + ' ' if @text.length > 1 && i != @text.length - 1
       output << convert(@text[i]) if @text.length == 1 || i == @text.length - 1
     end
-    output
   end
 
   def self.convert(val)
-    return val + 'ay' if @vowels.include?(val[0]) || val.start_with?('yt', 'xr')
+    return val + 'ay' if 'aioue'.include?(val[0]) || val.start_with?('yt', 'xr')
     pos = get_index(val)
     pos += 1 if val.start_with?('qu', 'squ')
     val.slice(pos..val.length - 1) + val.slice(0..pos - 1) + 'ay'
   end
 
   def self.get_index(str)
-    (0...str.length).each do |i|
-      return i if @vowels.include?(str[i])
-    end
+    (0...str.length).map { |i| return i if 'aioue'.include?(str[i]) }
   end
 end
