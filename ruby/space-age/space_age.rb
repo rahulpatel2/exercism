0bfd1d44080eb5267c9c8783948900968d30585f
Rahul Patel <rahulpatel.iiita@gmail.com>
2016-03-30 12:53:56 +0530

    Space age iteration 3

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,42 +1,20 @@
 # Program to Calculate Space age
 class SpaceAge
-  def initialize(seconds)
-    @age_in_seconds = seconds
-  end
+  attr_accessor :seconds
+  PLANETS = { 'on_mercury' => 0.2408467, 'on_venus' => 0.61519726,
+              'on_mars' => 1.8808158, 'on_jupiter' => 11.862615,
+              'on_saturn' => 29.447498, 'on_uranus' => 84.016846,
+              'on_neptune' => 164.79132 }.freeze
 
-  def seconds
-    @age_in_seconds
+  def initialize(seconds)
+    @seconds = seconds
   end
 
   def on_earth
-    @age_in_seconds.to_f / 31_557_600
+    @seconds.to_f / 31_557_600
   end
 
-  def on_mercury
-    on_earth / 0.2408467
-  end
-
-  def on_venus
-    on_earth / 0.61519726
-  end
-
-  def on_mars
-    on_earth / 1.8808158
-  end
-
-  def on_jupiter
-    on_earth / 11.862615
-  end
-
-  def on_saturn
-    on_earth / 29.447498
-  end
-
-  def on_uranus
-    on_earth / 84.016846
-  end
-
-  def on_neptune
-    on_earth / 164.79132
+  PLANETS.each do |method_name, value|
+    define_method(method_name) { on_earth / value }
   end
 end
