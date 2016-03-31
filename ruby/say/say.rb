55971f6bb6fd8f0fe526c7331ef011d99866cf62
Rahul Patel <rahulpatel.iiita@gmail.com>
2016-03-31 19:22:27 +0530

    number to say in ruby iteration 2

--- ruby/say/say.rb
+++ ruby/say/say.rb
@@ -31,41 +31,50 @@ class Say
                         30  => 'thirty',
                         20  => 'twenty'
                       }.freeze
+  LARGE_NUMBERS = {
+                    1_000_000_000 => 'billion',
+                    1_000_000 => 'million',
+                    1_000 => 'thousand',
+                    100 => 'hundred'
+                  }.freeze
+
   def initialize(num)
     raise ArgumentError if num < 0 || num >= 1_000_000_000_000
     @num = num
   end
 
   def in_english
-    say_for_large_numbers(@num)
+    say_for_all(@num)
   end
 
-  def say_twenty_to_hundred(number)
-    return TWENTY_TO_HUNDERD[number] unless TWENTY_TO_HUNDERD[number].nil?
-    TWENTY_TO_HUNDERD[(number / 10) * 10] + '-' + FIRST_TWENTY[number % 10]
+  def say_for_large(number, number_key)
+    return say_for_all(number / number_key) + ' ' + LARGE_NUMBERS[number_key] + ' ' +
+    say_for_all(number % number_key) if number % number_key != 0
+    FIRST_TWENTY[number / number_key] + ' ' + LARGE_NUMBERS[number_key]
   end
 
-  def say_for_large_numbers(number)
+  def say_for_more_than_hundred(number)
+    if number >= 100 && number < 1_000
+      return FIRST_TWENTY[number / 100] + ' hundred ' +
+      say_for_all(number % 100) if number % 100 != 0
+      FIRST_TWENTY[number / 100] + ' hundred'
+    elsif number >= 1_000 && number < 1_000_000
+      say_for_large(number, 1_000)
+    elsif number >= 1_000_000 && number < 1_000_000_000
+      say_for_large(number, 1_000_000)
+    else
+      say_for_large(number, 1_000_000_000)
+    end
+  end
+
+  def say_for_all(number)
     if number < 20
       FIRST_TWENTY[number]
     elsif number >= 20 && number < 100
-      say_twenty_to_hundred(number)
-    elsif number >= 100 && number < 1_000
-      return FIRST_TWENTY[number / 100] + ' hundred ' +
-      say_for_large_numbers(number % 100) if number % 100 != 0
-      FIRST_TWENTY[number / 100] + ' hundred'
-    elsif number >= 1_000 && number < 1_000_000
-      return say_for_large_numbers(number / 1000) + ' thousand ' +
-      say_for_large_numbers(number % 1000) if number % 1000 != 0
-      FIRST_TWENTY[number / 1000] + ' thousand'
-    elsif number >= 1_000_000 && number < 1_000_000_000
-      return say_for_large_numbers(number / 1_000_000) + ' million ' +
-      say_for_large_numbers(number % 1_000_000) if number % 1_000_000 != 0
-      FIRST_TWENTY[number / 1_000_000] + ' million'
+      return TWENTY_TO_HUNDERD[number] unless TWENTY_TO_HUNDERD[number].nil?
+      TWENTY_TO_HUNDERD[(number / 10) * 10] + '-' + FIRST_TWENTY[number % 10]
     else
-      return say_for_large_numbers(number / 1_000_000_000) + ' billion ' +
-      say_for_large_numbers(number % 1_000_000_000) if number % 1_000_000_000 != 0
-      FIRST_TWENTY[number / 1_000_000_000] + ' billion'
+      say_for_more_than_hundred(number)
     end
   end
 end
