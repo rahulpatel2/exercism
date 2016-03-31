4af6bbd5fd9f06fc4c89a8d6a346ad7cd57582aa
Rahul Patel <rahulpatel.iiita@gmail.com>
2016-03-31 13:54:16 +0530

    Sample cipher in ruby iteration 2

--- ruby/simple-cipher/simple_cipher.rb
+++ ruby/simple-cipher/simple_cipher.rb
@@ -6,29 +6,21 @@ class Cipher
       raise ArgumentError if key.empty? || key =~ /[A-Z]/ || key =~ /[0-9]/
       @key = key
     else
-      @key = createkey
+      @key = (0...20).map { ('a'..'z').to_a[rand(26)] }.join
     end
-  end
-
-  def createkey
-    (0...20).map { ('a'..'z').to_a[rand(26)] }.join
   end
 
   def encode(plaintext)
-    encode_text = ''
-    (0...plaintext.length).each do |i|
-      ascii_value = ((plaintext[i].ord - 97) + (key[i % key.length].ord - 97)) % 26 + 97
+    (0...plaintext.length).each_with_object('') do |i, encode_text|
+      ascii_value = ((plaintext[i].ord - 97) + (key[i].ord - 97)) % 26 + 97
       encode_text << ascii_value.chr
     end
-    encode_text
   end
 
   def decode(encode_text)
-    plaintext = ''
-    (0...encode_text.length).each do |i|
-      ascii_value = ((encode_text[i].ord - 97) - (key[i % key.length].ord - 97)) % 26 + 97
+    (0...encode_text.length).each_with_object('') do |i, plaintext|
+      ascii_value = ((encode_text[i].ord - 97) - (key[i].ord - 97)) % 26 + 97
       plaintext << ascii_value.chr
     end
-    plaintext
   end
 end
