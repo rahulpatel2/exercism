eb40b97adcfc34dc0a05129386203a7a086e670e
Rahul Patel <rahulpatel.iiita@gmail.com>
2016-04-04 10:38:40 +0530

    space age iteartion 4

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -4,17 +4,13 @@ class SpaceAge
   PLANETS = { 'on_mercury' => 0.2408467, 'on_venus' => 0.61519726,
               'on_mars' => 1.8808158, 'on_jupiter' => 11.862615,
               'on_saturn' => 29.447498, 'on_uranus' => 84.016846,
-              'on_neptune' => 164.79132 }.freeze
+              'on_neptune' => 164.79132, 'on_earth' => 1.00 }.freeze
 
   def initialize(seconds)
     @seconds = seconds
   end
 
-  def on_earth
-    @seconds.to_f / 31_557_600
-  end
-
-  PLANETS.each do |method_name, value|
-    define_method(method_name) { on_earth / value }
+  PLANETS.each do |age_on_plane, period|
+    define_method(age_on_plane) { @seconds.to_f / (31_557_600 * period) }
   end
 end
